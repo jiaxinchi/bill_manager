f7c128f926ffba69897ab591d6ed5e5d6eb7376a
贾新驰(工业软件) <jiaxinchi@t3caic.com>
2022-11-08 11:25:22 +0800

    修改存储文件命名方式

--- const.h
+++ const.h
@@ -1,7 +1,7 @@
 #define VERSION "1.0"
 #define MAXPL 16 //最大密码长度
-# define DFILENAME "BillData.bdf" //存储文件
-# define PSFILENAME "Password.bdf" //密码存储文件
+# define DFILENAME "billdata.bdf" //存储文件
+# define PSFILENAME "password.bdf" //密码存储文件
 # define dailybill 1
 # define socialbill 2
 # define businessbill 3
